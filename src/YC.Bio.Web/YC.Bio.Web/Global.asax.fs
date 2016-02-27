f4bea819e192073ca025ebf7f7dcbe943ac800da
rsdpisuy <rsdpisuy@gmail.com>
2016-02-27 14:37:19 +0300

    First template

---
+++ src/YC.Bio.Web/YC.Bio.Web/Global.asax.fs
@@ -0,0 +1,7 @@
+namespace YC.Bio.Web
+
+type Global() =
+    inherit System.Web.HttpApplication()
+
+    member g.Application_Start(sender: obj, args: System.EventArgs) =
+        ()
---
+++ src/YC.Bio.Web/YC.Bio.Web/Remoting.fs
@@ -0,0 +1,12 @@
+namespace YC.Bio.Web
+
+open WebSharper
+
+module Server =
+
+    [<Rpc>]
+    let DoSomething input =
+        let R (s: string) = System.String(Array.rev(s.ToCharArray()))
+        async {
+            return R input
+        }
